cb534f5101c1dbc685d6183c2096931be9f1d7c6
kanghanna1202 <namnam1178@daum.net>
2021-02-04 23:28:52 +0900

    [ADD] Create user create view

--- userSystem/views.py
+++ userSystem/views.py
@@ -1,9 +1,20 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 
-# Create your views here.
-from rest_framework.decorators import api_view
+from userSystem.models import User
+from userSystem.serializers import UserCreateSerializer
 
 
-@api_view(['GET'])
-def hello(request):
-    return "hello"
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def create_user(request):
+    serializer = UserCreateSerializer(data=request.data)
+    if not serializer.is_valid(raise_exception=True):
+        return Response({"message": "request error"}, status=status.HTTP_409_CONFLICT)
+
+    if User.objects.filter(email=serializer.validated_data['email']).first() is None:
+        serializer.save()
+        return Response({"message": "success"}, status=status.HTTP_201_CREATED)
+    return Response({"message": "duplicate email"}, status=status.HTTP_409_CONFLICT)
