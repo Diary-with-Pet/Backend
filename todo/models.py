eeceb7ac1d727e9132b44704072ccdd4160f5760
kanghanna1202 <namnam1178@daum.net>
2021-02-20 11:11:48 +0900

    [ADD] Create todo models

---
+++ todo/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class Todo(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user_id = models.ForeignKey("User", related_name="user", on_delete=models.CASCADE, db_column="user_id")
+    title = models.CharField(max_length=10)
+    content = models.CharField(max_length=30)
+    classification = models.IntegerField
