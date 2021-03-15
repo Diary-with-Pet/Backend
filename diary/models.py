210b27cc28af8b1ad3f53daf988628d7dec89f81
hanna <namnam1178@daum.net>
2021-03-15 15:07:07 +0900

    [FIX] Fix diary model - separate image model

--- diary/models.py
+++ diary/models.py
@@ -5,16 +5,13 @@ from userSystem.models import User
 
 class Diary(models.Model):
     id = models.BigAutoField(primary_key=True)
-    user = models.ForeignKey(User, related_name="diary", on_delete=models.CASCADE, null=True)
+    user = models.ForeignKey(User, related_name="diary_user", on_delete=models.CASCADE, null=True)
     title = models.CharField(max_length=30)
     content = models.CharField(max_length=800)
     date_created = models.DateField(auto_now_add=True)
 
-    image1 = models.ImageField(default='media/diary/default_image.jpeg', upload_to='diary',
-                               blank=True, null=True)
-    image2 = models.ImageField(default='media/diary/default_image.jpeg', upload_to='diary',
-                               blank=True, null=True)
-    image3 = models.ImageField(default='media/diary/default_image.jpeg', upload_to='diary',
-                               blank=True, null=True)
-    image4 = models.ImageField(default='media/diary/default_image.jpeg', upload_to='diary',
-                               blank=True, null=True)
+
+class DiaryImage(models.Model):
+    diary = models.ForeignKey(Diary, on_delete=models.CASCADE)
+    image = models.ImageField(default='media/diary/default_image.jpeg', upload_to='diary',
+                              blank=True, null=True)
