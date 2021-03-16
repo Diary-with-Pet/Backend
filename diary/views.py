ea53f0635d4232ece0ca3f8443d6d8b1536308e4
hanna <namnam1178@daum.net>
2021-03-16 19:29:05 +0900

    [FIX] Fix diary viewset

--- diary/views.py
+++ diary/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, status
+from rest_framework.response import Response
 
 from diary.serializers import DiarySerializer
 
@@ -9,3 +10,7 @@ class DiaryViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         return self.request.user.diary_user.all()
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+        return Response({"message": "success"}, status=status.HTTP_201_CREATED)
